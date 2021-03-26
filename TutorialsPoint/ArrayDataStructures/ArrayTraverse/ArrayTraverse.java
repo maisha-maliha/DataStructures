9d89704acd1a17786c433bb27161432651b3a1d9
maisha-maliha <officiallymaisha@gmail.com>
2021-03-26 21:19:41 +0600

    Array Traverse Data Structure coded

---
+++ TutorialsPoint/ArrayDataStructures/ArrayTraverse/ArrayTraverse.java
@@ -0,0 +1,11 @@
+public class ArrayTraverse{
+  public static void main(String[] args){
+    private int[] Num_Array = {55,94,856,7,45,4556,96,8,6};
+
+    System.out.println("All array elements:");
+    for(int index = 0; index < Num_Array.length; index++){
+      System.out.println("Array element at index " + index + ": " + Num_Array[index]);
+    }
+
+  }
+}
